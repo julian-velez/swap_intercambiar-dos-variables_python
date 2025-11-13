a3b1d4f03b2b0435cf45bd6f61e5de4642bed338
julian-velez <60607869+julian-velez@users.noreply.github.com>
2025-11-13 13:59:34 -0500

    swap_intercambiar dos variables_python
    swap_intercambiar dos variables_python

---
+++ swap/main.py
@@ -0,0 +1,14 @@
+# Se asignan valores iniciales a las variables
+# a tiene el valor 100 y b tiene el valor 200
+a, b = 100, 200
+
+# En esta línea ocurre un intercambio de valores:
+# a toma el valor de b (200) y b toma el valor de a (100)
+# Esto se logra gracias a la asignación múltiple en Python
+a, b = b, a
+
+# Se imprime el nuevo valor de a (que ahora es 200)
+print("a:", a)
+
+# Se imprime el nuevo valor de b (que ahora es 100)
+print("b:", b)
